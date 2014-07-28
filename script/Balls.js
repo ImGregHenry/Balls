752b3d0b3c088bce5ab247a68789cec30f07e7d1
Greg Henry <imsupergreg@live.com>
2014-07-27 22:10:37 -0400

    Danger-zone flagging added.  Tiling fixed.
    Fixed major tiling bug where empty tiles were not loading.  Character
    can now add danger-zone-tiles overtop of empty zone.  Danger-zone-tiles
    will clear to safe-zone-tiles after reaching a safe-zone again.

--- script/Balls.js
+++ script/Balls.js
@@ -1,46 +1,59 @@
 ﻿var NUMBER_OF_BALLS = 10;
-var PLAYER_VELOCITY = 90;
+const PLAYER_VELOCITY = 90;
+const TILE_WIDTH = 20;
+const TILE_HEIGHT = 20;
 
+const SAFE_ZONE_ID = 1;
+const DANGER_ZONE_ID = 2;
+const EMPTY_ZONE_ID = 3;
 
-var game = new Phaser.Game(1000, 800, Phaser.CANVAS, 'BALLS', { preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(1000, 800, Phaser.CANVAS, 'BALLS', { preload: preload, create: create, update: update });
 var balls;
 var cursors;
 var map;
 var layer;
-
 var playerTween = null;
-var playerNextTween;
-var playerTweenManager;
 
 function preload()
 {
-    //var TileMap = new TileMap(game, null, 5, 5, 50, 50);
-    game.load.image('ball', 'assets/star.png');
-    game.load.image('dude', 'assets/ball1.png');
-    game.load.image('tile-danger-zone', 'assets/tile-danger-zone.png');
-    game.load.image('tile-safe-zone', 'assets/tile-safe-zone.png');
-
     game.load.tilemap('map', 'assets/maps/TileMap.json', null, Phaser.Tilemap.TILED_JSON);
 
+    game.load.image('ball', 'assets/star.png');
+    game.load.image('dude', 'assets/ball1.png');
+    game.load.image('tile-danger-zone', 'assets/tile-danger-zone.png', true);
+    game.load.image('tile-safe-zone', 'assets/tile-safe-zone.png', true);
+    game.load.image('tile-empty', 'assets/tile-empty.png', true);
 }
+
+var map_tiles_wide;
+var map_tiles_tall;
 
 function create()
 {
     game.stage.backgroundColor = '#000000';
 
     map = game.add.tilemap('map');
-    map.addTilesetImage('tile-danger-zone');
-    map.addTilesetImage('tile-safe-zone');
-    map.setTileIndexCallback(255, this.awesomeEvent, this);
+    
     map.setCollisionBetween(1, 1);
+
+    map.addTilesetImage('tile-safe-zone', 'tile-safe-zone', 20, 20, 0, 0, 1);
+    map.addTilesetImage('tile-danger-zone', 'tile-danger-zone', 20, 20, 0, 0, 2);
+    map.addTilesetImage('tile-empty', 'tile-empty', 20, 20, 0, 0, 3);
     
     layer = map.createLayer('Tile Layer 1');
     
+    
+    map_tiles_wide = map.width;
+    map_tiles_tall = map.height;
+
+    console.log("width: " + map.width);
+    console.log("height: " + map.height);
+    
+
     layer.resizeWorld();
 
     //  We're going to be using physics, so enable the Arcade Physics system
     game.physics.startSystem(Phaser.Physics.ARCADE);
-
 
 
     player = game.add.sprite(0, 0, 'dude');
@@ -68,35 +81,89 @@ function create()
 
     // Load the keyboard controls
     cursors = game.input.keyboard.createCursorKeys();
-    //game.input.keyboard.onDownCallback = fillTiles;
-
-    playerTweenManager = new Phaser.TweenManager(game);
 }
 
-//function fillTiles()
-//{
-//    var tile = getCurrentTile();
+var isCharInDangerZone = false;
+var endangeredTiles = [];
 
-//    if (currentTile == null)
-//    {
-//        console.log("ERROR: Current tile is not found on the map.  ");
-//    }
+function fillTiles()
+{
+    
+    var x = getXTileIndex();
+    var y = getYTileIndex();
+
+    var currentTile = map.getTile(x, y, layer, false);
+    
+    if (currentTile == null)
+        console.log("ERROR: tile not found.");
+    else if(currentTile.index == EMPTY_ZONE_ID)
+    {
+        isCharInDangerZone = true;
+        map.fill(DANGER_ZONE_ID, x, y, 1, 1, layer);
+        endangeredTiles.push(x + "," + y);
+    }
+    else if (currentTile.index == DANGER_ZONE_ID)
+    {
+        //map.fill(SAFE_ZONE_ID, x, y, 1, 1);
+    }
+    else if(currentTile.index == SAFE_ZONE_ID)
+    {
+        if (isCharInDangerZone)
+        {
+            isCharInDangerZone = false;
+            clearEndangeredTiles();
+            
+        }
+    }
+
+    return currentTile;
+}
 
 
-//}
+function clearEndangeredTiles()
+{
+    console.log("clearing endangered tiles: " + endangeredTiles.length);
+    // Loop through all the endangered tiles and set them to SAFE_ZONE
+    for (var i = 0; i < endangeredTiles.length; i++)
+    {
+        // stored as ['1,1', '2,2', 3,5']
+        var vals = endangeredTiles[i].split(",");
+        console.log("Clearing: " + vals[0] + "," + vals[1] + " OF " + endangeredTiles.length);
+        
+        //map.removeTile(parseInt(vals[0]), parseInt(vals[1]), 1, 1);
+        
+        map.fill(SAFE_ZONE_ID, parseInt(vals[0]), parseInt(vals[1]), 1, 1, layer);
 
-//function getCurrentTile()
-//{
-//    var currentTile = map.getTile(player.body.x, player.body.y, layer, false);
+        var currentTile = map.getTile(parseInt(vals[0]), parseInt(vals[1]), layer, false);
+        currentTile.resetCollision();
+        
+    }
+    
+    
+    endangeredTiles = [];
+}
 
-//    return currentTile;
-//}
+
+
+function getXTileIndex()
+{
+    var position = player.body.x;
+
+    return Math.round(position / TILE_WIDTH);
+}
+
+function getYTileIndex()
+{
+    var position = player.body.y;
+
+    return Math.round(position / TILE_HEIGHT);
+}
+
 
 function startPlayerTween(x, y)
 {
     if (playerTween == null || !playerTween.isRunning)
     {
-        console.log("NewTween");
         playerTween = game.add.tween(player);
         //playerTween = new Phaser.Tween(player, game, playerTweenManager);
         playerTween.onComplete.add(playerTweenComplete, this);
@@ -109,6 +176,9 @@ function update()
 {
     game.physics.arcade.collide(balls, balls);
     game.physics.arcade.collide(balls, layer);
+
+    map.fill(DANGER_ZONE_ID, 3, 3, 1, 1);
+    map.fill(SAFE_ZONE_ID, 3, 3, 1, 1);
 
     // TODO: test whether move is valid or not before tweening
     if (cursors.left.isDown)
@@ -131,11 +201,11 @@ function update()
 
 function playerTweenComplete()
 {
-    console.log("tween complete");
+    fillTiles();
 }
 
 
-function render()
-{
+//function render()
+//{
 
-}
+//}
